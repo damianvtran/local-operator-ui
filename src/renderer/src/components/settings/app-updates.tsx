538af0443d89d7d0b3945fc43eb8e880b71edc7b
Damian Tran <damianvtran@gmail.com>
2025-03-16 18:14:17 -0400

    Separate check for updates button and update notification

--- src/renderer/src/components/settings/app-updates.tsx
+++ src/renderer/src/components/settings/app-updates.tsx
@@ -2,7 +2,7 @@ import { faDownload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Card, CardContent, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { CheckForUpdatesButton } from "../common/update-notification";
+import { CheckForUpdatesButton } from "../common/check-for-updates-button";
 
 const StyledCard = styled(Card)(() => ({
 	marginBottom: 32,
